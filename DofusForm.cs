b65ded88a5c907d5160b205d0e5a771a0806fc77
jordan nye <jordannye@hotmail.com>
2024-02-12 23:36:27 +0000

    WIP automatically downloading the next updated version

--- DofusForm.cs
+++ DofusForm.cs
@@ -4,10 +4,17 @@ using DofusSwap.Prefabs;
 using DofusSwap.Tray;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using DofusSwap.Properties;
+using static System.String;
+using System.IO.Compression;
+using Microsoft.Win32;
 
 namespace DofusSwap
 {
@@ -61,12 +68,18 @@ namespace DofusSwap
                 _KeyDown.Add(key,false);
             }
 
-            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
-            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
-            string version = fvi.FileVersion;
-            Text = $"Dofus Swap - {version}";
+            Text = Format(Resources.title_name, GetVersionString());
 
             _Initialising = false;
+
+            CheckForUpdate();
+        }
+
+        private string GetVersionString()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fvi.FileVersion;
         }
 
         private void AddCharacter(string displayName, Keys key, bool shift, bool control)
@@ -339,5 +352,79 @@ namespace DofusSwap
             return base.ProcessCmdKey(ref msg, keyData);
         }
 
+        private void checkUpdate_Click(object sender, EventArgs e)
+        {
+            CheckForUpdate();
+        }
+
+        private async void CheckForUpdate()
+        {
+            try
+            {
+                var currentVersionStr = GetVersionString();
+                string latestVersionStr = "";
+
+                var versionUrl = String.Format(Resources.update_url, currentVersionStr);
+
+                var client = new WebClient();
+                latestVersionStr = await client.DownloadStringTaskAsync(versionUrl);
+
+                if (!Version.TryParse(currentVersionStr, out Version currentVersion)) return;
+                if (!Version.TryParse(latestVersionStr, out Version latestVersion)) return;
+
+                //if (currentVersion >= latestVersion) return;
+
+                var dialogResult = MessageBox.Show($"Update {latestVersion} available, do you want to install it?", "DofusSwap",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    var downloadUrl = String.Format(Resources.download_url, latestVersionStr);
+                    var zipData = await client.DownloadDataTaskAsync(downloadUrl);
+
+                    var zip_path = Format(Resources.zip_download_path, GetDownloadFolderPath(), latestVersionStr);
+
+                    var zip_file_path = $"{zip_path}.zip";
+                    if (File.Exists(zip_file_path)) File.Delete(zip_file_path);
+                    File.WriteAllBytes(zip_file_path, zipData);
+
+                    if (Directory.Exists(zip_path))
+                    {
+                        RecursiveDelete(new DirectoryInfo(zip_path));
+                    }
+
+                    ZipFile.ExtractToDirectory(zip_file_path, zip_path);
+
+                    //Auto run the setup
+                    Process.Start($"{zip_path}\\SetupDofusSwap.exe");
+
+                    //Open explorer to the file
+                    //Process.Start("explorer.exe",$"{zip_path}");
+
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Download failed", "DofusSwap", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        string GetDownloadFolderPath() 
+        {
+            return Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
+        }
+
+        public static void RecursiveDelete(DirectoryInfo baseDir)
+        {
+            if (!baseDir.Exists)
+                return;
+
+            foreach (var dir in baseDir.EnumerateDirectories())
+            {
+                RecursiveDelete(dir);
+            }
+            baseDir.Delete(true);
+        }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,9 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace DofusSwap.Properties
-{
+namespace DofusSwap.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -18,52 +19,80 @@ namespace DofusSwap.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DofusSwap.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to https://raw.github.com/Glutoblop/DofusSwap/master/Downloadables/dofusswap_{0}.zip.
+        /// </summary>
+        internal static string download_url {
+            get {
+                return ResourceManager.GetString("download_url", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Version - {0}.
+        /// </summary>
+        internal static string title_name {
+            get {
+                return ResourceManager.GetString("title_name", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to https://raw.github.com/Glutoblop/DofusSwap/master/latest_version.txt.
+        /// </summary>
+        internal static string update_url {
+            get {
+                return ResourceManager.GetString("update_url", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0}\dofusswap_{1}.
+        /// </summary>
+        internal static string zip_download_path {
+            get {
+                return ResourceManager.GetString("zip_download_path", resourceCulture);
             }
         }
     }
